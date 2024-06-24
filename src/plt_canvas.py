f21dedf527278e9b5d81374b311f8105d8c1c9fc
Jakub Wilk <kubawilk63@gmail.com>
2024-06-24 16:07:17 +0200

    Use PltCanvas as a module

---
+++ src/plt_canvas.py
@@ -0,0 +1,9 @@
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.figure import Figure
+
+
+class PltCanvas(FigureCanvasQTAgg):
+    def __init__(self, width=5, height=4, dpi=100):
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = self.fig.add_subplot(111)
+        super(PltCanvas, self).__init__(self.fig)
