747efbf6fe3d4e07969ccd2ec78147932ce1fe3a
Jakub Wilk <kubawilk63@gmail.com>
2024-06-24 21:51:23 +0200

    Refactor utils

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,9 @@
 import cv2
-import random
 import numpy as np
+import torch
 
-SKELETON_COLORS = [
+# Define colors for each connection in skeleton
+SKELETON_COLORS = (
     (255, 0, 0),
     (0, 255, 0),
     (0, 0, 255),
@@ -23,29 +24,24 @@ SKELETON_COLORS = [
     (128, 255, 255),
     (255, 128, 64),
     (128, 64, 255)
-]
+)
 
 
-def draw_keypoints(landmarks, image, skeleton):
+def draw_keypoints(landmarks: torch.Tensor, image: np.array, skeleton: tuple) -> np.array:
+    # If landmarks list is empty return original image
+    if len(landmarks) == 0:
+        return image
+    # Extract every point
     for i, (start_point, end_point) in enumerate(skeleton):
-        if len(landmarks) == 0:
-            return image
+        # Skip point if it is 0
         if landmarks[start_point].all() == 0 or landmarks[end_point].all() == 0:
             continue
+
+        # If landmark exists draw it on image
         image = cv2.line(
             image, (int(landmarks[start_point][0].item()),
                     int(landmarks[start_point][1].item())),
             (int(landmarks[end_point][0].item()),
              int(landmarks[end_point][1].item())),
             SKELETON_COLORS[i], 4)
-
-    return image
-
-
-def draw_points(landmarks, image):
-    for i, landmark in enumerate(landmarks):
-        image = cv2.circle(
-            image, (int(landmark[0].item()), int(landmark[1].item())), 2, (255, 0, 0), 2)
-        image = cv2.putText(image, str(
-            i), (int(landmark[0].item()), int(landmark[1].item())), cv2.FONT_HERSHEY_COMPLEX, 1, color=(255, 0, 0))
     return image
