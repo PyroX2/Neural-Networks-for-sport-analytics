2b7c619aecf4d7295e9bb8232cf1ba692fb3497d
Jakub Wilk <kubawilk63@gmail.com>
2024-06-28 19:05:55 +0200

    Higher scaling

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,5 @@
 # Class for defining GUI settings
 class Settings:
     def __init__(self):
-        self.x_max_image_size = 500
-        self.y_max_image_size = 500
+        self.x_max_image_size = 700
+        self.y_max_image_size = 700
